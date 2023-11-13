31e0fca2f96abb17c314e6f6c1f629bfb526d817
CLEOSILVA <cleo_silva1906@hotmail.com>
2023-11-13 18:31:48 -0300

    Criando README

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -3,15 +3,6 @@ package org.example;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    // TODO: conhecer e importar a classe Scanner
-    // Exibir as mensagens para o nosso usuário
-    // Obter pela classe scanner os valores digitados no terminal
-    // Exibir a mensagem conta criada
-    // Número (conta) inteiro ok
-    // Agência String
-    // Nome do cliente String
-    // Saldo Double
-
     public static void main(String[] args) {
 
         double saldo = 237.48;
@@ -28,9 +19,5 @@ public class ContaTerminal {
         System.out.println("Olá " + cliente + ", obrigado por criar uma conta em" +
                 " nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo +
                 " já está disponível para saque.");
-
     }
-
-
-
 }
